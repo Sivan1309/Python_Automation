9639a4040b2214f5d7f4b6f06f48f426315e77aa
Sivan1309 <csrk007sathguru@gmail.com>
2025-11-05 12:09:09 +0000

    Initial-Commit

---
+++ Declaration.py
@@ -0,0 +1,19 @@
+def test():
+    #----------Variable Declarations and Data Types-----------------------
+    print("Hello, World!")
+    print("This is a test function.")
+    string_example = "This is a sample string."
+    number_example = 42 + 84 + 126\
+        + 168
+    list_example = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    dictionary_example = {"key1": "value1", "key2": "value2", "key3": "value3"}
+    tuple_example = (True, False, True, False)
+    set_example = {3.14, 2.71, 1.61, 0.577}
+    print(list_example)
+    print(dictionary_example)
+    print(tuple_example)
+    print(set_example)
+    print(string_example)
+    print(number_example)
+    
+test()
